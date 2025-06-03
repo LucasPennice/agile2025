2c7a1a3702cbbb14e0c613751c76bdb7fe05d4d1
FR4NKK0 <francoliberaca@gmail.com>
2025-06-02 21:53:02 -0300

    funcion mostrar progreso

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const PALABRA_A_ADIVINAR = "ESCALERA";
 
 export function adivinarLetra(letra, letrasYaAdivinadas = []) {
-  // if (letra  === "j"  letra === "J") return false
+  const letraMayus = letra.toUpperCase();
 
-  // if (letra  === "e"  letra === "E") return false
+  // Ya fue adivinada
+  if (letrasYaAdivinadas.includes(letraMayus)) return false;
 
-  const primeraAdivinanza = letrasYaAdivinadas.length === 0;
+  // Verificar si está en la palabra
+  return PALABRA_A_ADIVINAR.includes(letraMayus);
+}
 
-  if (!primeraAdivinanza)
-    return letrasYaAdivinadas.find((x) => x === letra) !== undefined;
+export function mostrarProgreso(palabra, letrasAdivinadas) {
+  const letras = palabra.split("");
 
-  return PALABRA_A_ADIVINAR.split("").find((x) => x === letra) !== undefined;
+  return letras
+    .map((letra) =>
+      letrasAdivinadas.map(l => l.toUpperCase()).includes(letra.toUpperCase())
+        ? letra.toUpperCase()
+        : "_"
+    )
+    .join(" ");
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { adivinarLetra } from ".";
+import { adivinarLetra, mostrarProgreso } from ".";
 
 describe("adivinarLetra", () => {
   it("test_adivina_si_tiene_j_no_tiene", () => {
@@ -13,12 +13,43 @@ describe("adivinarLetra", () => {
   it("test_no_se_puede_repetir_la_misma_letra", () => {
     const letrasYaAdivinadas = ["E", "D"];
 
-    expect(adivinarLetra("E", letrasYaAdivinadas)).toBe(true);
+    expect(adivinarLetra("E", letrasYaAdivinadas)).toBe(false);
+  });
+
+  it("debe ser insensible a mayúsculas/minúsculas", () => {
+    expect(adivinarLetra("e")).toBe(true);
+    expect(adivinarLetra("s")).toBe(true);
+    expect(adivinarLetra("j")).toBe(false);
   });
 });
+
+
 
 describe("adivinaPalabra", () => {
   it("test_adivina_si_tiene_j_no_tiene", () => {
     expect(adivinarLetra("J")).toBe(false);
+  });
+});
+
+
+describe("mostrarProgreso", () => {
+  it("test_no_se_adivino_ninguna_letra_muestra_guiones", () => {
+    const resultado = mostrarProgreso("ESCALERA", []);
+    expect(resultado).toBe("_ _ _ _ _ _ _ _");
+  });
+   it("test_se_muestran_letras_adivinadas_y_guiones", () => {
+    const resultado = mostrarProgreso("ESCALERA", ["E", "A"]);
+    expect(resultado).toBe("E _ _ A _ E _ A");
+  });
+
+  it("test_todas_las_letras_adivinadas_muestra_palabra_completa", () => {
+    const letras = Array.from(new Set("ESCALERA".split("")));
+    const resultado = mostrarProgreso("ESCALERA", letras);
+    expect(resultado).toBe("E S C A L E R A");
+  });
+
+  it("test_se_ignoran_mayusculas_minusculas_al_mostrar_progreso", () => {
+    const resultado = mostrarProgreso("ESCALERA", ["e", "a"]);
+    expect(resultado).toBe("E _ _ A _ E _ A");
   });
 });
